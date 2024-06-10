0a457f0dfcb6a17d8b3f4b9fc7657224c73b8a37
Gathekar98 <gathekar.ashwini@gmail.com>
2024-06-10 15:51:17 +0530

    calculator , color-changer , countdown timer projects done and  commit

---
+++ countdown timer/js/app.js
@@ -0,0 +1,18 @@
+const endDate = "23 May 2024 11:36:00 PM";
+document.getElementById('due-date').innerText = endDate;
+const inputs = document.querySelectorAll('input');
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end-now)/1000;
+    console.log(Math.floor(diff))
+    if (diff < 0) return;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+clock();
+setInterval(()=>{
+    clock()
+},1000);
